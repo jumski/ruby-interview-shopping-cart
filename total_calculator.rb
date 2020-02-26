2a87f284ddb5be4cb8cb3e163216bc3c2a779e44
Wojtek Majewski <jumski@gmail.com>
2020-02-26 16:26:51 +0100

    Improve naming of variables

--- total_calculator.rb
+++ total_calculator.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 class TotalCalculator
-  def initialize(items, promotions)
-    @items = items
+  def initialize(line_items, promotions)
+    @line_items = line_items
     @promotions = promotions
   end
 
   def calculate
-    promotions_in_processing_order.reduce(items) do |new_items, promotion|
-      promotion.apply(new_items)
+    promotions_in_processing_order.reduce(line_items) do |items, promotion|
+      promotion.apply(items)
     end.total
   end
 
   private
 
-  attr_reader :items, :promotions
+  attr_reader :line_items, :promotions
 
   def promotions_in_processing_order
     promotions.select(&:applies_to_item?) + promotions.select(&:applies_to_total?)
